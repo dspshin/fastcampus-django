0e89d96875d5e1c49fb0ab158a61e1068159cf28
ShinHo Chul <dsp@ShinHoui-MacBook-Pro.local>
2016-03-05 15:23:39 +0900

    03/05 ModelForm - custom clean

--- instablog/blog/forms.py
+++ instablog/blog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Post
+from django.forms import ValidationError
 
 class PostForm(forms.Form):
 	title = forms.CharField()
@@ -10,3 +11,16 @@ class PostEditForm(forms.ModelForm):
 		model = Post
 		fields = ('title', 'content', 'tags', )
 		#fields = '__all__'
+	def clean_title(self):
+		title = self.cleaned_data.get('title', '')
+		if '바보' in title:
+			raise ValidationError('invalid word')
+		return title.strip()
+	def clean(self):
+		# 마지막으로 전체적인 validation을 체크하도록 한다.
+		super(PostEditForm, self).clean()
+		title = self.cleaned_data.get('title', '')
+		content = self.cleaned_data.get('content', '')
+
+		if 'asd' in title:
+			self.add_error('title', 'asd cannot be included in title')
